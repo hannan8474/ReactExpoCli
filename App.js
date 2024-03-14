52429dec2d9fc60e0bdc057ec5bee3e09fee838f
unknown <raoabdulhannan757@gmail.com>
2024-03-15 02:55:13 +0500

    Mobile app with react expo cli

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("Hannan");
+  const changeName = () => {
+    setName("Abdul Hannan");
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={styles.container}>Here is the data of our user</Text>
+      <UserData title={name}/>
+      <View>
+        <Button title='Update State' onPress={() => changeName()} />
+      </View>
     </View>
   );
 }
 
+
+
+const UserData = (props) => {
+  const myFunc = () => {
+    console.warn('Function called');
+  }
+
+  return (
+    <>
+      <View style={styles.data}>
+        <Text style={{ textAlign: 'center' }}>
+        name: {props.title}
+        </Text>
+        <Text style={{ textAlign: 'center' }}>
+          age: 21
+        </Text>
+      </View>
+
+      <View>
+        <Button title='Press Me' color='red' onPress={myFunc} />
+      </View>
+    </>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ff5',
+    color: 'red',
+    fontSize: 24,
+    margin: 20,
   },
+  data: {
+    backgroundColor: 'green',
+    height: 500,
+    display: 'flex',
+    justifyContent: 'center',
+
+  }
 });
